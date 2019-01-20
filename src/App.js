93d9c28da23336c213ecac83f05e1f23e565b912
christophwottawa <christoph.wottawa@me.com>
2019-01-20 16:31:43 +0100

    Added basic navbar with links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import React, {Component, Fragment} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Switch, Route} from 'react-router-dom';
 
-import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
 class App extends Component {
   render() {
     return (
       <Fragment>
         <Navbar/>
-        <ProductList/>
-        <Details/>
-        <Cart/>
-        <Default/>
+        <Switch>
+          <Route exact path="/" component={ProductList}/>
+          <Route path="/details" component={Details}/>
+          <Route path="/cart" component={Cart}/>
+          <Route component={Default}/>
+        </Switch>
       </Fragment>
     );
   }
