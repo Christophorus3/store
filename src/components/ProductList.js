1147f46ae2082ab672f7e85d20998497e61f538d
christophwottawa <christoph.wottawa@me.com>
2019-01-21 13:52:54 +0100

    implemented Context API for ProductList and created context.js

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,23 +2,34 @@ import React, {Component, Fragment} from 'react';
 import Product from "./Product";
 import Title from "./Title";
 
+import { storeProducts } from "../data";
+import { ProductConsumer } from '../context'
+
 class ProductList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      products: []
+      products: storeProducts
     }
   }
 
   render() {
+    console.log(this.state.products);
+
     return (
       <Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products"/>
             <div className="row">
-
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product}/>
+                  });
+                }}
+              </ProductConsumer>
             </div>
           </div>
         </div>
---
+++ src/context.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+import {storeProducts, detailProduct} from "./data";
+
+const ProductContext = React.createContext();
+//Provider
+
+//Consumer
+
+class ProductProvider extends Component {
+  state = {
+    products: storeProducts,
+    productDetail: detailProduct
+  };
+
+  handleDetail = () => {
+    console.log("Hello from detail");
+  };
+
+  addToCart = () => {
+    console.log("add to cart")
+  }
+  render() {
+    return (
+      <ProductContext.Provider value={{
+        ...this.state,
+        handleDetail: this.handleDetail,
+        addToCart: this.addToCart
+      }}>
+        {this.props.children}
+      </ProductContext.Provider>
+    );
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export {ProductProvider, ProductConsumer};
