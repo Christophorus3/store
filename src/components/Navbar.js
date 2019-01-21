6f923c68a8ba291cd7cef0536e9b29da7e6d378f
christophwottawa <christoph.wottawa@me.com>
2019-01-21 10:06:05 +0100

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import Button from './Button';
 
@@ -7,7 +8,7 @@ import logo from '../logo.svg';
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/*
         https://www.iconfinder.com/icons/1243689/call_phone_icon
         Creative Commons (Attribution 3.0 Unported);
@@ -30,9 +31,19 @@ class Navbar extends Component {
             my cart
           </Button>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: uppercase !important;
+  }
+`;
 
 export default Navbar;
