c2a0d06b5992df15e5181094a022bf5165247bb9
christophwottawa <christoph.wottawa@me.com>
2019-01-21 14:05:01 +0100

    cleanup ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,18 +2,9 @@ import React, {Component, Fragment} from 'react';
 import Product from "./Product";
 import Title from "./Title";
 
-import { storeProducts } from "../data";
 import { ProductConsumer } from '../context'
 
 class ProductList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: storeProducts
-    }
-  }
-
   render() {
     console.log(this.state.products);
 
