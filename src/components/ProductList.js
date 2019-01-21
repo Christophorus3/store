7760ba2d24ab92b92053cab8865ee8a4e0e4239d
christophwottawa <christoph.wottawa@me.com>
2019-01-21 11:29:25 +0100

    Added Title to Product page

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,28 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Product from "./Product";
+import Title from "./Title";
 
 class ProductList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: []
+    }
+  }
+
   render() {
     return (
-      <div>
-        <h3>This is ProductList.</h3>
-        <Product/>
-      </div>
+      <Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products"/>
+            <div className="row">
+
+            </div>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
